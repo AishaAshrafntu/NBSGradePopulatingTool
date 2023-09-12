5b2c1199918fa790d44fb4643073d12852783a98
Aisha Ashraf <aisha.ashraf@ntu.ac.uk>
2023-09-12 14:24:06 +0100

    package related changes

--- NBSGradePopulatingTool/Controllers/HomeController.cs
+++ NBSGradePopulatingTool/Controllers/HomeController.cs
@@ -1,9 +1,10 @@
-﻿using IronXL;
-using UploadFile.Models;
+﻿using UploadFile.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using Microsoft.AspNetCore.Authorization;
 using ClosedXML.Excel;
+using ExcelDataReader;
+using System.IO;
 
 
 namespace NBSGradePopulatingTool.Controllers
@@ -52,10 +53,10 @@ namespace NBSGradePopulatingTool.Controllers
                     file.BannerXlsxFile.CopyTo(bannerXlsxStream);
                 }
                 string fileName = Path.GetFileNameWithoutExtension(bannerXlsxFilePath) + "-completed.xlsx";
+
                 using (XLWorkbook wb = new XLWorkbook())
                 {
                     DataTable dt = this.ProcessCsvAndXlsx(nowCsvFilePath, bannerXlsxFilePath).Tables[0];
-
                     wb.Worksheets.Add(dt);
                     using (MemoryStream stream = new MemoryStream())
                     {
@@ -80,11 +81,29 @@ namespace NBSGradePopulatingTool.Controllers
         {
             try
             {
-                WorkBook csvWorkbook = WorkBook.Load(csvFilePath);
-                WorkBook xlsxWorkbook = WorkBook.Load(xlsxFilePath);
+                System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                FileStream stream = System.IO.File.Open(xlsxFilePath, FileMode.Open, FileAccess.Read);
+                IExcelDataReader excelReader;
 
-                var csvSheet = csvWorkbook.DefaultWorkSheet;
-                var xlsxSheet = xlsxWorkbook.DefaultWorkSheet;
+                //1. Reading Excel file
+                if (Path.GetExtension(xlsxFilePath).ToUpper() == ".XLS")
+                {
+                    //1.1 Reading from a binary Excel file ('97-2003 format; *.xls)
+                    excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+                }
+                else
+                {
+                    //1.2 Reading from a OpenXml Excel file (2007 format; *.xlsx)
+                    excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                }
+
+                var dataSet = excelReader.AsDataSet(new ExcelDataSetConfiguration
+                {
+                    ConfigureDataTable = _ => new ExcelDataTableConfiguration
+                    {
+                        UseHeaderRow = true // Use first row is ColumnName here :D
+                    }
+                });
 
                 var nowCsvLines = System.IO.File.ReadAllLines(csvFilePath);
                 for (int i = 1; i < nowCsvLines.Length; i++) // Skip header
@@ -95,8 +114,8 @@ namespace NBSGradePopulatingTool.Controllers
                         string gradeSymbol = nowCsvValues[5]; // Assuming Grade Symbol is at index 5
                         string nNumber = nowCsvValues[4]; // Assuming N Number is at index 4
 
-                        var matchingRow = xlsxSheet.Rows
-                            .FirstOrDefault(x => x.Columns[3].Value.ToString() == nNumber);
+
+                        DataRow matchingRow = dataSet.Tables[0].AsEnumerable().Where(r => ((string)r["Student ID"]).Contains(nNumber)).First();
 
                         if (matchingRow != null)
                         {
@@ -105,44 +124,40 @@ namespace NBSGradePopulatingTool.Controllers
                                 string[] gradeParts = gradeSymbol.Split('-');
                                 string leftPart = gradeParts[0];
 
-
-
                                 if (leftPart == "0")
                                 {
-                                    matchingRow.Columns[10].Value = "ZERO"; // Assuming Grade is at index 10
+                                    matchingRow["Grade"] = "ZERO"; // Assuming Grade is at index 10
                                 }
                                 else
                                 {
-                                    matchingRow.Columns[10].Value = leftPart; // Assuming Grade is at index 10
+                                    matchingRow["Grade"] = leftPart; // Assuming Grade is at index 10
                                 }
                                 string rightPart = gradeParts[1];
 
-
-
                                 if (rightPart == "Capped" || rightPart == "DL")
                                 {
-                                    matchingRow.Columns[14].Value = "DL"; // Assuming Grade Change is at index 14
-                                    matchingRow.Columns[15].Value = ""; // Assuming Comment is at index 15
+                                    matchingRow["Grade Change Reason"] = "DL"; // Assuming Grade Change is at index 14
+                                    matchingRow["Comment"] = ""; // Assuming Comment is at index 15
                                 }
                                 else if (rightPart == "NE" || rightPart == "NN" || rightPart == "NS" || rightPart == "NK")
                                 {
-                                    matchingRow.Columns[14].Value = ""; // Assuming Grade Change is at index 14
-                                    matchingRow.Columns[15].Value = rightPart.Substring(0, 2); // Assuming Comment is at index 15
+                                    matchingRow["Grade Change Reason"] = ""; // Assuming Grade Change is at index 14
+                                    matchingRow["Comment"] = rightPart.Substring(0, 2); // Assuming Comment is at index 15
                                 }
                                 else
                                 {
-                                    matchingRow.Columns[14].Value = ""; // Assuming Grade Change is at index 14
-                                    matchingRow.Columns[15].Value = rightPart; // Assuming Comment is at index 15
+                                    matchingRow["Grade Change Reason"] = ""; // Assuming Grade Change is at index 14
+                                    matchingRow["Comment"] = rightPart; // Assuming Comment is at index 15
                                 }
                             }
                             else
                             {
-                                matchingRow.Columns[10].Value = gradeSymbol; // Assuming Grade is at index 10
+                                matchingRow["Grade"] = gradeSymbol; // Assuming Grade is at index 10
                             }
                         }
                     }
                 }
-                System.Data.DataSet dataSet = xlsxWorkbook.ToDataSet(true); // Allow easy integration with DataGrids, SQL and EF
+                excelReader.Close();
                 return dataSet;
             }
             catch (Exception ex)
